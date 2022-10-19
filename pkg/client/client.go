2a141320cbf84d2886bf52b519fb1b06eeae4aa0
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2022-10-19 16:57:15 -0300

    refactor: add websocket client

---
+++ pkg/client/client.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func Upgrade(writer http.ResponseWriter, request *http.Request) (*websocket.Conn, error) {
+	ws, err := upgrader.Upgrade(writer, request, nil)
+
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+
+	return ws, nil
+}
+
+func Reader(connection *websocket.Conn) {
+	for {
+		messageType, message, err := connection.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(message))
+
+		if err := connection.WriteMessage(messageType, message); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func Writer(connection *websocket.Conn) {
+	for {
+		fmt.Println("Sending")
+		messageType, reader, err := connection.NextReader()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		w, err := connection.NextWriter(messageType)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if _, err := io.Copy(w, reader); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if err := w.Close(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
