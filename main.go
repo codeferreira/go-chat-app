3c663e15c2597886924bd5f043b158d17b2a68c4
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2022-10-19 16:58:05 -0300

    refactor: serve ws to use client pkg

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/codeferreira/realtime-chat-app/pkg/client"
 )
+
+func serverWs(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println(request.Host)
+
+	ws, err := client.Upgrade(writer, request)
+
+	if err != nil {
+		fmt.Println(writer, "%+V\n", err)
+	}
+
+	go client.Writer(ws)
+	client.Reader(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
