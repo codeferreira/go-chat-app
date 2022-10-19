7080b4245b4b806595e00f1c6b08df3c23e489e6
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2022-10-19 17:19:05 -0300

    feat: add start pool function

--- pkg/websocket/client.go
+++ pkg/websocket/client.go
@@ -37,3 +37,33 @@ func (client *Client) Read() {
 		fmt.Printf("Message Received: %+v\n", message)
 	}
 }
+
+func (pool *Pool) Start() {
+	for {
+			select {
+			case client := <-pool.Register:
+					pool.Clients[client] = true
+					fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+					for client := range pool.Clients {
+							fmt.Println(client)
+							client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+					}
+					break
+			case client := <-pool.Unregister:
+					delete(pool.Clients, client)
+					fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+					for client := range pool.Clients {
+							client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+					}
+					break
+			case message := <-pool.Broadcast:
+					fmt.Println("Sending message to all clients in Pool")
+					for client := range pool.Clients {
+							if err := client.Conn.WriteJSON(message); err != nil {
+									fmt.Println(err)
+									return
+							}
+					}
+			}
+	}
+}
